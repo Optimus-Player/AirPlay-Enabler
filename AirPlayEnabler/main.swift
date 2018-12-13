f78fb8fa1d09818414ab88ae195dcdf7d8522fc8
Darren Mo <contact@optimusplayer.com>
2018-12-12 17:14:27 -0800

    Set up project to build a privileged helper tool

---
+++ AirPlayEnabler/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  AirPlayEnabler
+//
+//  Created by Darren Mo on 2018-12-11.
+//  Copyright © 2018 Darren Mo. All rights reserved.
+//
+
+import os
+
+os_log("AirPlayEnabler is running.")
